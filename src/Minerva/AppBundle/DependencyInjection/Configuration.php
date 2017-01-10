4bd0e81cf4a63eb4f1c96c53bd70bbe5b20d41eb
Pascal Vervest <pascalvervest@gmail.com>
2017-01-10 23:46:05 +0100

    correct root node name

--- src/Minerva/AppBundle/DependencyInjection/Configuration.php
+++ src/Minerva/AppBundle/DependencyInjection/Configuration.php
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('prezent_api');
+        $rootNode = $treeBuilder->root('minerva_app');
 
         return $treeBuilder;
     }
